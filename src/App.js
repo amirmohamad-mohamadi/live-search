4be41bd9e660e2182c62e9925a6f9b7dde4aca88
amirmohamad-mohamadi <76755078+amirmohamad-mohamadi@users.noreply.github.com>
2022-01-26 16:00:38 +0330

    create file navbar and with react-router-dom and rect-icons

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
+import { BrowserRouter as Router } from "react-router-dom";
+
+import Navbar from "./components/Layout/Navbar";
+
 function App() {
   return (
-    <div>
+    <Router>
+      <div className="flex flex-col justify-between  h-screen">
+        <Navbar />
+        <main className="">Content</main>
+      </div>
       <h1>Hello World</h1>
-    </div>
+    </Router>
   );
 }
 
