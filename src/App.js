11b205d9b0aca538b012b71255c88969b6d00079
amirmohamad-mohamadi <76755078+amirmohamad-mohamadi@users.noreply.github.com>
2022-01-26 16:46:15 +0330

    create footer

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import { BrowserRouter as Router } from "react-router-dom";
 
 import Navbar from "./components/Layout/Navbar";
+import Footer from "./components/Layout/Footer";
 
 function App() {
   return (
     <Router>
       <div className="flex flex-col justify-between  h-screen">
         <Navbar />
-        <main className="">Content</main>
+        <main className="container mx-auto pb-12">Content</main>
+        <Footer />
       </div>
       <h1>Hello World</h1>
     </Router>
--- src/components/Layout/Navbar.jsx
+++ src/components/Layout/Navbar.jsx
@@ -4,7 +4,7 @@ import { FaGithub } from "react-icons/fa";
 
 const Navbar = ({ title }) => {
   return (
-    <nav className="navbar mb-12 shadow-lg bg-neutral text-neutral-content">
+    <nav className="navbar mb-12 shadow-lg bg-neutral text-blue-content">
       <div className="container mx-auto">
         <div className="flex-none px-2 text-3xl">
           <FaGithub className="inline pr-2 text-3xl" />
